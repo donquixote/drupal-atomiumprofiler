ba60cfbf54bd2fa75c8172d331cb25b76e4d452d
Andreas Hennings <andreas@dqxtech.net>
2018-08-29 23:22:50 +0200

    Enhance ProfilingCase_AtomiumAttributesToString.

--- src/ProfilingCase/ProfilingCase_AtomiumAttributesToString.php
+++ src/ProfilingCase/ProfilingCase_AtomiumAttributesToString.php
@@ -13,7 +13,12 @@ class ProfilingCase_AtomiumAttributesToString extends ProfilingCase_AtomiumBase 
    * Returns nothing.
    */
   public function run() {
-    $original = new Attributes(['class' => ['xxx', 'yyy'], 'selected' => TRUE]);
+    $original = new Attributes(
+      [
+        'class' => ['xxx', 'yyy'],
+        'selected' => TRUE,
+        'href' => ['https://github.com/ec-europa/atomium'],
+      ]);
     for ($i = 1000; $i > 0; --$i) {
       $attributes = clone $original;
       $attributes->__toString();
