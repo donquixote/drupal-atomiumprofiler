9ded670e6fa7fd741bb866a44ef8b06c599bd336
Andreas Hennings <andreas@dqxtech.net>
2018-09-14 12:59:56 +0200

    Add ProfilingCase_ThemeHook with example theme hooks.

---
+++ src/ProfilingCase/ProfilingCase_ThemeHook.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\atomiumprofiler\ProfilingCase;
+
+use Drupal\cfrprofiler\ProfilingCase\ProfilingCaseInterface;
+
+class ProfilingCase_ThemeHook implements ProfilingCaseInterface {
+
+  /**
+   * @var string
+   */
+  private $hook;
+
+  /**
+   * @var array
+   */
+  private $variables;
+
+  /**
+   * @CfrPlugin(
+   *   "atomiumprofiler_example_with_element",
+   *   "theme('atomiumprofiler_example_with_element', *)"
+   * )
+   *
+   * @return self
+   */
+  public static function exampleWithElement() {
+    return new self(
+      'atomiumprofiler_example_with_element',
+      ['element' => []]);
+  }
+
+  /**
+   * @CfrPlugin(
+   *   "atomiumprofiler_example_with_variables",
+   *   "theme('atomiumprofiler_example_with_variables', *)"
+   * )
+   *
+   * @return self
+   */
+  public static function exampleWithVariables() {
+    return new self(
+      'atomiumprofiler_example_with_variables',
+      ['text' => 'hello']);
+  }
+
+  /**
+   * @CfrPlugin(
+   *   "atomiumprofiler_example_minimal",
+   *   "theme('atomiumprofiler_example_minimal', *)"
+   * )
+   *
+   * @return self
+   */
+  public static function exampleMinimal() {
+    return new self(
+      'atomiumprofiler_example_minimal',
+      []);
+  }
+
+  /**
+   * @param string $hook
+   * @param array $variables
+   */
+  public function __construct($hook, array $variables) {
+    $this->hook = $hook;
+    $this->variables = $variables;
+  }
+
+  /**
+   * Clears static caches etc.
+   *
+   * This method does not count on profiling time.
+   */
+  public function reset() {
+    // Reset nothing.
+  }
+
+  /**
+   * No return value.
+   *
+   * @throws \Exception
+   */
+  public function run() {
+    for ($i = 10000; $i > 0; --$i) {
+      theme($this->hook, $this->variables);
+    }
+  }
+}
